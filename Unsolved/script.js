e611dd3591ddc33ec9037602cd72bb6356df88e1
Tom Bond <tombond248@gmail.com>
2022-10-06 20:28:17 +0100

    complete

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -1,1 +1,9 @@
 //WRITE YOUR CODE BELOW
+
+var students = ["Tom", "Amy", "Kenny", "James", "Bob"]
+
+console.log(students.length);
+
+for(var i = 0; i < students.length; i++) {
+ console.log("great to see you " + students [i] + "!")
+}
